d27421b217f76779eaa3b7c6238d4394ca7b1b5c
Priya Ravi <priyaravi23@gmail.com>
2020-01-20 18:03:36 -0800

    Serve a webpack Bundle while Developing with webpack-dev-server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,8 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-    mode: 'development' // non-minified version
+    mode: 'development', // non-minified version
+    devServer: {
+        port: 9000
+    }
 });
